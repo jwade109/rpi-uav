85ac483be60c2f6fc1e6a3f5481c8b324e382cf1
Wade Foster <jwade109@vt.edu>
2017-09-06 00:38:04 -0400

    Revamped kalman filter+test

--- spike/kalman/kalman.h
+++ spike/kalman/kalman.h
@@ -9,7 +9,9 @@ class kalman
     public:
 
     Eigen::Matrix<rep, N, 1> X;
+    Eigen::Matrix<rep, M, 1> Z;
     Eigen::Matrix<rep, N, N> P;
+    Eigen::Matrix<rep, N, M> G;
     Eigen::Matrix<rep, N, N> F;
     Eigen::Matrix<rep, M, N> H;
     Eigen::Matrix<rep, M, M> R;
@@ -20,21 +22,32 @@ class kalman
     const Eigen::Matrix<rep, N, 1>& step(const Eigen::Matrix<rep, M, 1>& Z);
 };
 
+// default constructor
 template <size_t M, size_t N, typename rep>
-kalman<M, N, rep>::kalman() { }
+kalman<M, N, rep>::kalman() :
+    kalman(Eigen::Matrix<rep, N, 1>::Zero())
+{ }
 
+// constructor with initial state
 template <size_t M, size_t N, typename rep>
 kalman<M, N, rep>::kalman(const Eigen::Matrix<rep, N, 1>& initial) :
-    X(initial) { }
+    X(initial),
+    Z(Eigen::Matrix<rep, M, 1>::Zero()),
+    P(Eigen::Matrix<rep, N, N>::Identity()),
+    F(Eigen::Matrix<rep, N, N>::Identity()),
+    H(Eigen::Matrix<rep, M, N>::Constant(1)),
+    R(Eigen::Matrix<rep, M, M>::Identity()),
+    Q(Eigen::Matrix<rep, N, N>::Identity())
+{ }
 
 template <size_t M, size_t N, typename rep>
 const Eigen::Matrix<rep, N, 1>& kalman<M, N, rep>::
-    step(const Eigen::Matrix<rep, M, 1>& Z)
+    step(const Eigen::Matrix<rep, M, 1>& meas)
 {
+    Z = meas;
     X = F * X;
     P = F * P * F.transpose() + Q;
-    Eigen::MatrixXd G = (P * H.transpose()) *
-        (H * P * H.transpose() + R).inverse();
+    G = (P * H.transpose()) * (H * P * H.transpose() + R).inverse();
     X = X + G * (Z - H * X);
     P = (Eigen::MatrixXd::Identity(N, N) - G * H) * P;
     return X;
--- spike/kalman/main.cpp
+++ spike/kalman/main.cpp
@@ -1,14 +1,99 @@
 #include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <time.h>
+#include <signal.h>
+#include <math.h>
+
 #include "kalman.h"
 
-int main()
+using namespace std;
+using namespace std::chrono;
+using namespace Eigen;
+
+bool cont = true;
+
+void sigint(int signal)
 {
-    Eigen::Matrix<double, 2, 1> init = {2, 3};
-    std::cout << init << std::endl;
-    kalman<3, 2> k(init);
-    auto x = {2, 4, -1};
-    std::cout << x << std::endl;
-    std::cout << k.step(x) << std::endl;
+    cont = false;
+}
+
+template <int M, int N, typename rep>
+const string println(const Matrix<rep, M, N>& m)
+{
+    stringstream s;
+    s << fixed;
+    s << setprecision(12);
+    for (int i = 0; i < m.rows(); i++)
+        for (int j = 0; j < m.cols(); j++)
+            s << setw(15) << m(i, j) << " ";
+    return s.str();
+}
+
+template <size_t M, size_t N, typename rep>
+void print(const kalman<M, N, rep>& k)
+{
+    std::cout
+        << "X: " << println(k.X) << std::endl
+        << "Z: " << println(k.Z) << std::endl
+        << "P: " << println(k.P) << std::endl
+        << "G: " << println(k.G) << std::endl
+        << "F: " << println(k.F) << std::endl
+        << "H: " << println(k.H) << std::endl
+        << "R: " << println(k.R) << std::endl
+        << "Q: " << println(k.Q) << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    signal(SIGINT, sigint);
+
+    vector<double> raw1, raw2, error;
+
+    srand(time(0));
+    const size_t M = 2, N = 1;
+    kalman<M, N, double> kf;
+
+    kf.R *= 3;
+    kf.Q *= 0.2;
+    kf.P *= 100;
+
+    std::cout << "[" << std::flush;
+
+    int max = 10000, bars = max/80;
+    for (int i = 0; i < max & cont; i++)
+    {
+        // system("clear");
+        VectorXd z = VectorXd::Constant(M, 10) + VectorXd::Random(M);
+        // print(kf);
+        kf.step(z);
+
+        raw1.push_back(kf.Z(0));
+        raw2.push_back(kf.Z(1));
+        error.push_back(kf.X(0));
+        if (i % bars == 0) std::cout << "|" << std::flush;
+        // this_thread::sleep_for(milliseconds(150));
+    }
+    std::cout << "]" << std::endl;
+
+    double avg[3] = {0, 0, 0};
+    for (double e : raw1)
+        avg[0] += (e - 10) * (e - 10);
+    for (double e : raw2)
+        avg[1] += (e - 10) * (e - 10);
+    for (double e : error)
+        avg[2] += (e - 10) * (e - 10);
+    for (double& e : avg)
+        e = sqrt(e/raw1.size());
+
+    std::cout << "Done.\nRMS: ";
+    for (double e : avg)
+        std::cout << e << " ";
+    std::cout << std::endl;
+
     return 0;
 }
