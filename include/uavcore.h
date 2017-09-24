2a7ef88927c3060cf140a6ee9a3a08339947e26a
Wade Foster <jwade109@vt.edu>
2017-09-24 01:15:42 -0400

    Add format definitions, rename serialize functions
    This commit adds a function namespace, uav::fmt, containing formatting
    bitmasks for printing states.
    
    to_binary() and from_binary() were also renamed to serialize() and
    deserialize(), and ::bin objects now use bytes, a typedef for unsigned
    char.
    
    Additionally, to make params aggregate initializeable again, freq_t is
    now a typedef for uint8_t instead of an enum -- be warned, this means
    params can pass an invalid or zero frequency

--- include/uavcore.h
+++ include/uavcore.h
@@ -27,7 +27,9 @@ namespace uav
     using mrate_t       = uint16_t;
     using wgt_frac_t    = double;
 
-    enum freq_t : uint8_t
+    using byte = unsigned char;
+
+    enum
     {
         f1hz = 1, f10hz = 10, f20hz = 20, f25hz = 25, f40hz = 40, f50hz = 50,
         f100hz = 100, f125hz = 125, f200hz = 200, f250hz = 250,
@@ -35,12 +37,35 @@ namespace uav
         fdefault = f100hz
     };
 
+    // formatting bitmasks /////////////////////////////////////////////////////
+
+    namespace fmt
+    {
+        using bitmask_t = uint32_t;
+
+        enum : bitmask_t
+        {
+            time = 1,
+            time_full = 7,
+            pressure = 3 << 3,
+            temperature = 3 << 5,
+            altitude = 1 << 7,
+            attitude = 7 << 8,
+            attitude_full = 15 << 8,
+            targets = 15 << 12,
+            pid = 15 << 16,
+            motors = 15 << 20,
+            error = 1 << 24,
+
+            all = std::numeric_limits<bitmask_t>::max(),
+            standard = time | altitude | attitude | motors | error
+        };
+    }
+
     // uav::state //////////////////////////////////////////////////////////////
 
-    class state
+    struct state
     {
-        public:
-
         timestamp_t     t, t_abs, comptime; // time in millis, computation time
         pres_t          pres[2];            // pressure from imu/bmp
         temp_t          temp[2];            // temperature from above
@@ -52,15 +77,16 @@ namespace uav
         pid_ov_t        zov, hov, pov, rov; // respective pid response
         motor_t         motors[4];
         error_t         err;                // bitmask for storing error codes
-    
-        const static size_t fields = 25;
-   
-        const static size_t size = 3*sizeof(timestamp_t) + 2*sizeof(pres_t) +
-            2*sizeof(temp_t) + sizeof(alt_t) + 3*sizeof(attitude_t) +
-            sizeof(calib_t) + 4*sizeof(target_t) + 4*sizeof(pid_ov_t) +
-            4*sizeof(motor_t) + sizeof(error_t);
 
-        using bin = std::array<uint8_t, size>;
+        static std::string header(fmt::bitmask_t);
+
+        const static size_t fields = 25;
+        const static size_t size = 3 * sizeof(timestamp_t) + 2 * sizeof(pres_t) +
+            2 * sizeof(temp_t) + sizeof(alt_t) + 3 * sizeof(attitude_t) +
+            sizeof(calib_t) + 4 * sizeof(target_t) + 4 * sizeof(pid_ov_t) +
+            4 * sizeof(motor_t) + sizeof(error_t);
+
+        using bin = std::array<byte, size>;
     };
 
     static_assert(state::fields == 25, "CONST_STATE_FIELDS_NOT_24");
@@ -68,30 +94,27 @@ namespace uav
 
     // uav::param //////////////////////////////////////////////////////////////
 
-    class param
+    struct param
     {
-        public:
-
         freq_t          freq;               // frequency of updates in hz
         home_pres_t     p1h, p2h;           // home point pres from imu/bmp
 
         // pid gains for altitude, heading, pitch, roll
         pid_gain_t      zpidg[4], hpidg[4], ppidg[4], rpidg[4];
-        
+
         lpf_tau_t       gz_rc;              // RC time constant for alt lpf
         wavg_t          gz_wam;             // weighted average gain towards z1
         mrate_t         maxmrate;           // max thrust d/dt in hz
         wgt_frac_t      mg;                 // vehicle weight/max thrust * 100
 
-        static param zero();
-    
-        const static size_t fields = 23;
+        static std::string header();
 
-        const static size_t size = sizeof(freq_t) + 2*sizeof(home_pres_t) +
-            16*sizeof(pid_gain_t) + sizeof(lpf_tau_t) + sizeof(wavg_t) +
+        const static size_t fields = 23;
+        const static size_t size = sizeof(freq_t) + 2 * sizeof(home_pres_t) +
+            16 * sizeof(pid_gain_t) + sizeof(lpf_tau_t) + sizeof(wavg_t) +
             sizeof(mrate_t) + sizeof(wgt_frac_t);
 
-        using bin = std::array<uint8_t, size>;
+        using bin = std::array<byte, size>;
     };
 
     static_assert(param::fields == 23, "CONST_PARAM_FIELDS_NOT_23");
@@ -99,31 +122,27 @@ namespace uav
 
     // conversion functions ////////////////////////////////////////////////////
 
-    param::bin to_binary(const param& p);
+    param::bin serialize(const param& p);
 
-    state::bin to_binary(const state& s);
+    state::bin serialize(const state& s);
 
-    param from_binary(const param::bin& b);
+    param deserialize(const param::bin& b);
 
-    state from_binary(const state::bin& b);
+    state deserialize(const state::bin& b);
 
     template<class T, class U> std::array<uint8_t, T::size> wrap(U *ptr)
     {
-        std::array<uint8_t, T::size> bin;
-        uint8_t *src = reinterpret_cast<uint8_t*>(ptr);
+        std::array<byte, T::size> bin;
+        byte* src = static_cast<byte*>(ptr);
         std::copy(src, src + T::size, bin.begin());
         return bin;
     }
 
     // std::string functions ///////////////////////////////////////////////////
 
-    std::string pheader();
-
-    std::string sheader(uint64_t mask);
-
     std::string to_string(const param& prm);
 
-    std::string to_string(const state& it, uint64_t mask);
+    std::string to_string(const state& it, fmt::bitmask_t mask);
 
     std::string timestamp();
 
