2848e08df3dc46b718e047484e208b675c69721f
Wade Foster <jwade109@vt.edu>
2017-10-18 18:10:37 -0400

    Add integral class
    integral<N> is a template class which basically just integrates
    N times. It's pretty self explanatory

--- spike/derivative/main.cpp
+++ spike/derivative/main.cpp
@@ -10,7 +10,7 @@ template <uint8_t N> class derivative
     const double dt;
     const static int order = N;
 
-    derivative<N>(uint8_t freq) : value(0), freq(freq),
+    derivative<N>(uint8_t freq = 1) : value(0), freq(freq),
         dt(1.0/freq), current(NAN), samples(0), d_dx(freq) { };
 
     double step(double sample)
@@ -42,6 +42,40 @@ template <> class derivative<0>
     double operator () (double x) { return x; }
 };
 
+template <uint8_t N> class integral
+{
+    public:
+    
+    double value;
+    const uint8_t freq;
+    const double dt;
+    const static int order = N;
+    
+    integral<N>(uint8_t freq = 1) : value(0), freq(freq),
+        dt(1.0/freq), sum_dx(freq) { }
+        
+    double step(double sample)
+    {
+        return (value += sum_dx(sample) * dt);
+    }
+    
+    double operator () (double x) { return step(x); }
+    
+    private:
+    
+    integral<N-1> sum_dx;
+};
+
+template <> class integral<0>
+{
+    public:
+    
+    integral(uint8_t) { };
+    
+    double step(double sample) { return sample; }
+    double operator () (double x) { return x; }
+};
+
 int main()
 {
     const int freq = 200;
@@ -67,6 +101,22 @@ int main()
             << d3(y(x)) << "\t"
             << d4(y(x)) << "\t"
             << d5(y(x)) << std::endl;
+    }
+    
+    integral<1> s1(freq);
+    integral<2> s2(freq);
+    integral<3> s3(freq);
+    
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "x\ts1\ts2\ts3" << std::endl;
+    
+    double x = 0;
+    for (; x < 10; x+=dt)
+    {
+        std::cout << x << "\t"
+            << s1(x) << "\t"
+            << s2(x) << "\t"
+            << s3(x) << std::endl; 
     }
     return 0;
 }
